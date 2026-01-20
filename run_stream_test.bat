636091229d87296dc190aa89d39657656c2c0278
Nathaniel Nathaniel <nathaniel_sigit@yahoo.com>
2026-01-20 03:10:32 +0100

    update

--- run_stream_test.bat
+++ run_stream_test.bat
@@ -1,4 +1,7 @@
 start cmd /k run.bat
 TIMEOUT /t 3
-start cmd /k "cd /d %~dp0\test && uvicorn test_stream:app --port 10003"
-start cmd /k python -m worker.main --target-hostname http://localhost:10003
+start cmd /k "cd /d %~dp0\test && set SERVER_NAME=Server 1 && uvicorn test_stream:app --port 20001"
+start cmd /k python -m worker.main --target-hostname http://localhost:20001
+
+start cmd /k "cd /d %~dp0\test && set SERVER_NAME=Server 2 && uvicorn test_stream:app --port 20002"
+start cmd /k python -m worker.main --target-hostname http://localhost:20002
--- test/test_stream.py
+++ test/test_stream.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 import time
+import os
+
+SERVER_NAME = os.getenv("SERVER_NAME", "Server")
 
 app = FastAPI()
 
 def event_stream():
     for i in range(1, 20):
-        yield f"data: Streaming data {i}\n\n"  # SSE format
+        yield f"{SERVER_NAME}: Streaming data {i}\n\n"  # SSE format
         time.sleep(1)
 
 @app.get("/")
